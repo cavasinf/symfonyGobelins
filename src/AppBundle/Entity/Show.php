b3ba487e55cf729c9f2776571d97a2a6ee7601b5
Pauline <schnee.pauline@gmail.com>
2018-02-05 16:34:29 +0100

    commit fin cours 05 fev

---
+++ src/AppBundle/Entity/Show.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Entity;
+
+class Show
+{
+    private $id;
+    private $name;
+    private $category;
+    private $abstract;
+    private $country;
+    private $author;
+    private $releaseDate;
+    private $mainPicture;
+
+}
